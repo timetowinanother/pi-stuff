b2a0e535e61789972d7dc95bd517b62fb4a725d1
timetowinanother <spotch@gmail.com>
2024-04-14 11:00:54 -0400

    Update logcpu.py
    1.) previously the C temp and F temp columns didn't match because each was populated by two temp calls taken at separate times, which resulted in some instances where the C went down (from the previous measurement) but the F went up, and vice versa. Now the F temp is just a conversion of whatever the C temp is, which is also one less temp call being made, which should make the program a smidge faster and more accurate. 
    
    2.) added a check to see what governor the pi is using.

--- logcpu.py
+++ logcpu.py
@@ -18,10 +18,10 @@ from time import sleep, strftime, time
 cpu = CPUTemperature()
 
 
-## 0:time, 1:temp, 2:volts, 3-6:core frequencies, 7:fahreneheit, 8: used memory MBs
-def write_temp(temp,volts,cpu0freq,cpu1freq,cpu2freq,cpu3freq,ftemp,memused):
+## 0:time, 1:temp, 2:volts, 3-6:core frequencies, 7:fahreneheit, 8: scaling governor, 9: used memory MBs
+def write_temp(temp,volts,cpu0freq,cpu1freq,cpu2freq,cpu3freq,ftemp,gov,memused):
     with open("./cpustats.csv", "a") as log:
-        log.write("{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp), str(volts[1][5:]), str(cpu0freq), str(cpu1freq), str(cpu2freq), str(cpu3freq),str(ftemp),str(memused)))
+        log.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp), str(volts[1][5:]), str(cpu0freq), str(cpu1freq), str(cpu2freq), str(cpu3freq),str(ftemp),str(gov),str(memused)))
 
 
 ## Enable for graphing functionality.
@@ -42,9 +42,10 @@ while True:
     cpu1freq = os.popen('cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq').read().strip()
     cpu2freq = os.popen('cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq').read().strip()
     cpu3freq = os.popen('cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq').read().strip()
-    ftemp = round((((cpu.temperature) * 9 / 5 ) + 32),2)
+    ftemp = round((((temp) * 9 / 5 ) + 32),2)
+    gov = os.popen('cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor').read().strip()
     memused = round(int(os.popen('cat /proc/meminfo | grep Active: | tr -d -c 0-9').read().strip())/1000,2)
-    write_temp(temp,volts,cpu0freq,cpu1freq,cpu2freq,cpu3freq,ftemp,memused)
+    write_temp(temp,volts,cpu0freq,cpu1freq,cpu2freq,cpu3freq,ftemp,gov,memused)
     sleep(10)
 #   graph(temp)
 #   plt.pause(10)
